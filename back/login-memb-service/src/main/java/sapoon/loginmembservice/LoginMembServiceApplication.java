6e62be48ea511c0a59429710c8c59f6e788d500b
Wonhyeong Cho <tmzkdls7989@gmail.com>
2020-05-24 14:09:20 +0900

    first commit

---
+++ back/login-memb-service/src/main/java/sapoon/loginmembservice/LoginMembServiceApplication.java
@@ -0,0 +1,13 @@
+package sapoon.loginmembservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMembServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginMembServiceApplication.class, args);
+    }
+
+}
---
+++ back/login-memb-service/src/test/java/sapoon/loginmembservice/LoginMembServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sapoon.loginmembservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMembServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

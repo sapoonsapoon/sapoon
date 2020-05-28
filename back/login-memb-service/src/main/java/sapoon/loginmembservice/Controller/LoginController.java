84fb0ce3f99e0151d9edb057bcd36ed5878edf9b
Wonhyeong Cho <tmzkdls7989@gmail.com>
2020-05-28 17:30:27 +0900

    gradle 설정 변경

--- back/login-memb-service/src/main/java/sapoon/loginmembservice/Controller/LoginController.java
+++ back/login-memb-service/src/main/java/sapoon/loginmembservice/Controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sapoon/login")
 public class LoginController {
 
-    @RequestMapping(method = RequestMethod.POST, value = "")
+    @RequestMapping(method = RequestMethod.GET, value = "")
     public String login(){
         return "hello";
     }

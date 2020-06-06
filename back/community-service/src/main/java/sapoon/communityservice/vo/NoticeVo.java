ff939807d8fa71399df08bb103fef3d7bfdb4380
Wonhyeong Cho <tmzkdls7989@gmail.com>
2020-06-06 15:55:24 +0900

    커뮤니티 서비스 게시물 생성 함수 만듦

---
+++ back/community-service/src/main/java/sapoon/communityservice/vo/NoticeVo.java
@@ -0,0 +1,24 @@
+package sapoon.communityservice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class NoticeVo {
+    private int seq;
+    private String title;
+    private String comment;
+    private String fileYn;
+    private String imgYn;
+    private String useYn;
+    private int viewCount;
+    private String regId;
+    private Date regDate;
+    private String updId;
+    private Date updDate;
+}

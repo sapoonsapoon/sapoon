02844bbd212967ed840d247aaeab3fc10534f471
육천호 <justyuk@naver.com>
2020-06-11 15:20:33 +0900

    #로그 설정, 이력 관련 로직 구현

--- back/login-memb-service/src/main/java/com/sapoon/loginmembservice/controller/LoginMemberController.java
+++ back/login-memb-service/src/main/java/com/sapoon/loginmembservice/controller/LoginMemberController.java
@@ -3,6 +3,8 @@ package com.sapoon.loginmembservice.controller;
 import com.sapoon.loginmembservice.common.exception.InvalidDataException;
 import com.sapoon.loginmembservice.vo.MemberInfoVO;
 import com.sapoon.loginmembservice.service.LoginMemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,13 +16,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/sapoon")
 public class LoginMemberController {
 
+    Logger logger = LoggerFactory.getLogger(LoginMemberController.class);
+
     @Autowired
     private LoginMemberService loginMemberService;
 
     @PostMapping("/login")
     public ResponseEntity<?> getLogin(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/login");
         try{
-            return ResponseEntity.ok().headers(getTokenHeader()).body(loginMemberService.login(memberInfoVO));
+            return ResponseEntity.ok().headers(getTokenHeader(loginMemberService.login(memberInfoVO))).build();
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(getErrorHeader()).body(e.getMessage());
         }
@@ -28,6 +33,8 @@ public class LoginMemberController {
 
     @GetMapping("member/overlap/{id}")
     public ResponseEntity<?> idOverlapCheck(@PathVariable("id") String id){
+        logger.info("loginMemberController - sapoon/member/overlap/" + id);
+
         try{
             loginMemberService.selectId(id);
             return ResponseEntity.ok().build();
@@ -38,6 +45,7 @@ public class LoginMemberController {
 
     @PostMapping("member/regist")
     public ResponseEntity<?> insertMember(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/member/regist");
         try{
             loginMemberService.insertMember(memberInfoVO);
             return ResponseEntity.ok().build();
@@ -48,6 +56,7 @@ public class LoginMemberController {
 
     @PostMapping("member/modify/password")
     public ResponseEntity<?> updatePassword(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/member/modify/password");
         try{
             loginMemberService.updatePassword(memberInfoVO);
             return ResponseEntity.ok().build();
@@ -58,6 +67,7 @@ public class LoginMemberController {
 
     @PostMapping("member/modify/info")
     public ResponseEntity<?> updateInfo(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/member/modify/info");
         try{
             loginMemberService.updateInfo(memberInfoVO);
             return ResponseEntity.ok().build();
@@ -68,6 +78,7 @@ public class LoginMemberController {
 
     @GetMapping("member/get/{id}")
     public ResponseEntity<?> getMember(@PathVariable("id") String id){
+        logger.info("loginMemberController - sapoon/member/get/" + id);
         try{
             return ResponseEntity.ok().headers(getSuccessHeader()).body(loginMemberService.selectMember(id));
         }catch (InvalidDataException e){
@@ -77,6 +88,7 @@ public class LoginMemberController {
 
     @PostMapping("member/find/id")
     public ResponseEntity<?> getId(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/member/find/id");
         try{
             return ResponseEntity.ok().headers(getSuccessHeader()).body(loginMemberService.selectIdUsingNameEmailBirthday(memberInfoVO));
         }catch (InvalidDataException e){
@@ -87,6 +99,7 @@ public class LoginMemberController {
 
     @PostMapping("member/find/password")
     public ResponseEntity<?> changePw(@RequestBody MemberInfoVO memberInfoVO){
+        logger.info("loginMemberController - sapoon/member/find/password");
         try{
             return ResponseEntity.ok().headers(getSuccessHeader()).body(loginMemberService.FindPassword(memberInfoVO));
         }catch (InvalidDataException e){
@@ -106,9 +119,9 @@ public class LoginMemberController {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         return httpHeaders;
     }
-    private HttpHeaders getTokenHeader() {
+    private HttpHeaders getTokenHeader(String token) {
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.TEXT_PLAIN);
+        httpHeaders.set("Authorization",token);
         return httpHeaders;
     }
 

c3256810b2e3c204ddf5cfd897f3d5e02623d017
육천호 <justyuk@naver.com>
2020-06-14 09:32:02 +0900

    #JWT 인터셉트 설정 수정

--- back/login-memb-service/src/main/java/com/sapoon/loginmembservice/config/WebConfig.java
+++ back/login-memb-service/src/main/java/com/sapoon/loginmembservice/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATHS = {
             "/sapoon/member/regist",
-            "/sapoon/login",
+            "/sapoon/member/login",
             "/sapoon/member/find/*",
             "/sapoon/member/overlap/*"
     };

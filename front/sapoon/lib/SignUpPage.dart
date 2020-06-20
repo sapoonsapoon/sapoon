214485b876b841adb1807e8ec34fba59ad8a1bc4
백상우 <qortkdn@naver.com>
2020-06-20 15:13:42 +0900

    화면 구성 개발완료
    화면 구성 완료

--- front/sapoon/lib/SignUpPage.dart
+++ front/sapoon/lib/SignUpPage.dart
@@ -23,6 +23,8 @@ class _SignUpPageState extends State<SignUpPage> {
   TextEditingController _nicknameController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -32,35 +34,35 @@ class _SignUpPageState extends State<SignUpPage> {
 
   var geolocator = Geolocator();
   var locationOptions =
-  LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+      LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
 
-  String show_log='생일을 입력해주세요';
+  String show_log = '생일을 입력해주세요';
 
   String dropdownValue = '    남자';
 
   void _incrementCounter(String value) {
     setState(() {
-      show_log =value;
+      show_log = value;
       print(show_log);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-
         appBar: AppBar(
-          title: Text('회원가입 화면', textAlign: TextAlign.right,),
-
+          title: Text(
+            '회원가입 화면',
+            textAlign: TextAlign.right,
+          ),
         ),
         body: Builder(builder: (BuildContext context) {
-            return SingleChildScrollView(
-
-              child: SafeArea(
-        child:  Center(
-                child: Column(
+          return SingleChildScrollView(
+            child: SafeArea(
+              child: Form(
+              key: _formKey,
+                child: Center(
+                    child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
@@ -73,12 +75,15 @@ class _SignUpPageState extends State<SignUpPage> {
                       child: TextFormField(
                         style: TextStyle(
                             fontFamily: "NanumSquareExtraBold",
-                            color: Colors.black45, fontWeight: FontWeight.bold),
+                            color: Colors.black45,
+                            fontWeight: FontWeight.bold),
                         textAlignVertical: TextAlignVertical.bottom,
                         controller: _idController,
                         decoration: InputDecoration(
                           hintText: '아이디',
-                          hintStyle: TextStyle(color: Colors.black45,),
+                          hintStyle: TextStyle(
+                            color: Colors.black45,
+                          ),
                           enabledBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                               color: Colors.black12,
@@ -95,7 +100,6 @@ class _SignUpPageState extends State<SignUpPage> {
                           filled: true,
                           prefixIcon: Icon(Icons.account_circle),
                         ),
-
                         validator: (String value) {
                           //pw input 조건
                           if (value.isEmpty) {
@@ -114,7 +118,8 @@ class _SignUpPageState extends State<SignUpPage> {
                       child: TextFormField(
                         style: TextStyle(
                             fontFamily: "NanumSquareExtraBold",
-                            color: Colors.black45, fontWeight: FontWeight.bold),
+                            color: Colors.black45,
+                            fontWeight: FontWeight.bold),
                         obscureText: true,
                         controller: _pwController,
                         decoration: InputDecoration(
@@ -136,7 +141,6 @@ class _SignUpPageState extends State<SignUpPage> {
                           filled: true,
                           prefixIcon: Icon(Icons.https),
                         ),
-
                         validator: (String value) {
                           //pw input 조건
                           if (value.isEmpty) {
@@ -155,7 +159,8 @@ class _SignUpPageState extends State<SignUpPage> {
                       child: TextFormField(
                         style: TextStyle(
                             fontFamily: "NanumSquareExtraBold",
-                            color: Colors.black45, fontWeight: FontWeight.bold),
+                            color: Colors.black45,
+                            fontWeight: FontWeight.bold),
                         controller: _pwControllerCf,
                         decoration: InputDecoration(
                           hintText: '비밀번호 확인 ',
@@ -197,10 +202,11 @@ class _SignUpPageState extends State<SignUpPage> {
                           child: TextFormField(
                             style: TextStyle(
                                 fontFamily: "NanumSquareExtraBold",
-                                color: Colors.black45, fontWeight: FontWeight.bold),
+                                color: Colors.black45,
+                                fontWeight: FontWeight.bold),
                             controller: _nameController,
                             decoration: InputDecoration(
-                             hintText: '이름 ',
+                              hintText: '이름 ',
                               hintStyle: TextStyle(color: Colors.black45),
                               enabledBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
@@ -228,38 +234,41 @@ class _SignUpPageState extends State<SignUpPage> {
                           ),
                         ),
                         Padding(
-                          padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.01),
+                          padding: EdgeInsets.only(
+                              right: MediaQuery.of(context).size.width * 0.01),
                         ),
                         Container(
                           width: MediaQuery.of(context).size.width * 0.367,
                           height: MediaQuery.of(context).size.width * 0.13,
                           decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black12,)
-                          ),
+                              border: Border.all(
+                            color: Colors.black12,
+                          )),
                           child: DropdownButton<String>(
                             underline: Text(''),
                             isExpanded: true,
                             value: dropdownValue,
                             iconSize: 24,
                             hint: Container(
-                                alignment: Alignment.centerRight,
-                                width: 100,
-                                  child: Text("Hint text", textAlign: TextAlign.end),
-
+                              alignment: Alignment.centerRight,
+                              width: 100,
+                              child: Text("Hint text", textAlign: TextAlign.end),
                             ),
-                            style: TextStyle(color: Colors.black45, fontWeight: FontWeight.bold,
-                              fontFamily: "NanumSquareExtraBold",
-                            fontSize: 16),
+                            style: TextStyle(
+                                color: Colors.black45,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "NanumSquareExtraBold",
+                                fontSize: 16),
                             onChanged: (String newValue) {
                               setState(() {
                                 dropdownValue = newValue;
                               });
                             },
-                            items: <String>['    남자','    여자']
+                            items: <String>['    남자', '    여자']
                                 .map<DropdownMenuItem<String>>((String value) {
                               return DropdownMenuItem<String>(
                                 value: value,
-                                child: Text(value,textAlign: TextAlign.right),
+                                child: Text(value, textAlign: TextAlign.right),
                               );
                             }).toList(),
                           ),
@@ -274,9 +283,7 @@ class _SignUpPageState extends State<SignUpPage> {
                         DatePicker.showDatePicker(
                           context,
                           showTitleActions: true,
-                          onChanged: (date) {
-
-                          },
+                          onChanged: (date) {},
                           onConfirm: (date) {
                             print('완료 $date');
                             String sss = DateFormat("yyyy-MM-dd").format(date);
@@ -285,15 +292,15 @@ class _SignUpPageState extends State<SignUpPage> {
                           },
                           currentTime: DateTime.now(),
                           locale: LocaleType.ko,
-
                         );
                       },
                       child: Container(
                         width: MediaQuery.of(context).size.width * 0.744,
                         height: MediaQuery.of(context).size.width * 0.12,
                         decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black12,)
-                        ),
+                            border: Border.all(
+                          color: Colors.black12,
+                        )),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -390,18 +397,26 @@ class _SignUpPageState extends State<SignUpPage> {
                     ),
                     InkWell(
                       onTap: () {
-                        final snackbar = SnackBar(content: Text('계정이 없습니다.'));
-                        Scaffold.of(context).showSnackBar(snackbar);
-                        createAlbum(context,
-                           _idController.text,
-                         _pwController.text,
-                         _nameController.text,
-                         _sexController.text,
-                         _nicknameController.text,
-                       _emailController.text,
-                        show_log,
-                          show_log,
-                        );
+                        if(_formKey.currentState.validate()){
+                          createAlbum(
+                            context,
+                            _idController.text,
+                            _pwController.text,
+                            _nameController.text,
+                            _sexController.text,
+                              show_log,
+                            _emailController.text,
+                            _nicknameController.text,
+
+
+                          );
+                        }else{
+                          final snackbar = SnackBar(content: Text('계정이 없습니다.'));
+                          Scaffold.of(context).showSnackBar(snackbar);
+                        }
+
+
+
                       },
                       child: Container(
                         decoration: BoxDecoration(
@@ -424,14 +439,14 @@ class _SignUpPageState extends State<SignUpPage> {
                     ),
                   ],
                 )),
-      ),
-            );
-    }));
+              ),
+            ),
+          );
+        }));
   }
 }
 
 InputDecoration getTextFieldDecor(String hint) {
-
   return InputDecoration(
     hintText: hint,
     hintStyle: TextStyle(color: Colors.black45),
@@ -452,17 +467,15 @@ InputDecoration getTextFieldDecor(String hint) {
   );
 }
 
-
-
-Future createAlbum(BuildContext context,
+Future createAlbum(
+    BuildContext context,
     String id,
     String password,
     String name,
     String gender,
     String birthday,
     String email,
-    String nickname,
-    String registPath) async {
+    String nickname) async {
   final http.Response response = await http.post(
     'http://35.194.192.57/sapoon/member/regist',
     headers: <String, String>{
@@ -470,10 +483,10 @@ Future createAlbum(BuildContext context,
     },
     body: jsonEncode(<String, String>{
       'id': '$id',
-      'password':"$password",
-      'name':'$name',
-      'gender':'$gender',
-      'birthday':'$birthday',
+      'password': "$password",
+      'name': '$name',
+      'gender': '$gender',
+      'birthday': '$birthday',
       'email': '$email',
       'nickname': '$nickname',
     }),
@@ -482,7 +495,6 @@ Future createAlbum(BuildContext context,
     // If the server did return a 201 CREATED response,
     // then parse the JSON.
     print('성공적이에요!');
-
   } else {
     print(name);
     print('Response status: ${response.statusCode}');
@@ -492,4 +504,3 @@ Future createAlbum(BuildContext context,
     throw Exception();
   }
 }
-

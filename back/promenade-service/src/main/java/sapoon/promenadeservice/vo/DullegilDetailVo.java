24344e12e89eae24c961baec27db86f8f3f121f9
Wonhyeong Cho <tmzkdls7989@gmail.com>
2020-07-03 21:20:58 +0900

    산책로 서비스 DB snake -> camel 설정 변경

--- back/promenade-service/src/main/java/sapoon/promenadeservice/vo/DullegilDetailVo.java
+++ back/promenade-service/src/main/java/sapoon/promenadeservice/vo/DullegilDetailVo.java
@@ -8,9 +8,10 @@ import lombok.ToString;
 @Setter
 @ToString
 public class DullegilDetailVo {
+    private int seq;
     private String thumbnail;
-    private String img_path1;
-    private String img_path2;
-    private String img_path3;
-    private String img_path4;
+    private String imgPath1;
+    private String imgPath2;
+    private String imgPath3;
+    private String imgPath4;
 }

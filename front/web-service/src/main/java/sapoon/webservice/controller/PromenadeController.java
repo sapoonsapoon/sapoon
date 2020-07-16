e14431e85cec5180623d0c3a2899fac19e5f7992
sixthou <justyuk@naver.com>
2020-07-16 21:27:19 +0900

    #로그추가

--- front/web-service/src/main/java/sapoon/webservice/controller/PromenadeController.java
+++ front/web-service/src/main/java/sapoon/webservice/controller/PromenadeController.java
@@ -1,18 +1,23 @@
 package sapoon.webservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.runtime.Log;
 
 @Controller
 @RequestMapping("/sapoon/web/promenade")
 public class PromenadeController {
+    private static final Logger logger = LoggerFactory.getLogger(PromenadeController.class);
     /*
         인덱스 페이지
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello() {
+        logger.info("****hello****");
         return "index";
     }
 }
